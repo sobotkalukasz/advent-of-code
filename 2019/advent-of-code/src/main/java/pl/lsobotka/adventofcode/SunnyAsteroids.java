c55c9e15a301f6fab349bc7fd575c9ce1427995c
sobotkalukasz <sobotka.lukasz@gmail.com>
2021-01-05 20:37:08 +0100

    solution for AoC 2019 'Day 5: Sunny with a Chance of Asteroids' Part 2 - refactor

--- 2019/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SunnyAsteroids.java
+++ 2019/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SunnyAsteroids.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/*
+ * https://adventofcode.com/2019/day/5
+ * */
 public class SunnyAsteroids {
 
     private static final int STOP = 99;
@@ -24,84 +28,67 @@ public class SunnyAsteroids {
     Deque<Integer> input;
     List<Integer> output;
     int[] program;
+    AtomicInteger index;
 
 
     public SunnyAsteroids(int[] instructions, Integer... input) {
         program = instructions;
         this.input = Stream.of(input).collect(Collectors.toCollection(LinkedList::new));
         this.output = new ArrayList<>();
+        index = new AtomicInteger(-1);
     }
 
     public List<Integer> execute() {
-        int opIndex = -1;
-        while (program[++opIndex] != STOP) {
-            opIndex = executeOperation(opIndex);
+        while (program[index.incrementAndGet()] != STOP) {
+            executeOperation();
         }
         return output;
     }
 
-    private int executeOperation(int index) {
-        return isSimpleOperation(index) ? executeSimple(index) : executeComplex(index);
+    private void executeOperation() {
+        int opCode = getCurrentOpCode();
+        int[] mode = getParamMode();
+        if (opCode == INPUT) setValueByIndex(index.incrementAndGet(), input.removeFirst(), mode[0]);
+        else if (opCode == OUTPUT) output.add(getValueByIndex(index.incrementAndGet(), mode[0]));
+        else {
+            int arg1 = getValueByIndex(index.incrementAndGet(), mode[0]);
+            int arg2 = getValueByIndex(index.incrementAndGet(), mode[1]);
+            if (isJumpOperation(opCode)) {
+                if (opCode == JUMP_IF_TRUE && arg1 != 0) index.set(arg2 - 1);
+                if (opCode == JUMP_IF_FALSE && arg1 == 0) index.set(arg2 - 1);
+            } else {
+                int result = getResult(opCode, arg1, arg2);
+                setValueByIndex(index.incrementAndGet(), result, mode[2]);
+            }
+        }
+    }
+
+    private int getCurrentOpCode() {
+        if (isSimpleOperation(index.get()))
+            return program[index.get()];
+        String op = String.valueOf(program[index.get()]);
+        return Integer.parseInt(op.substring(op.length() - 2));
+    }
+
+    private int[] getParamMode() {
+        int[] mode = new int[]{POSITION_MODE, POSITION_MODE, POSITION_MODE};
+        if (!isSimpleOperation(index.get())) {
+            String op = String.valueOf(program[index.get()]);
+            for (int i = op.length() - 3, j = 0; i >= 0; i--, j++) {
+                mode[j] = Integer.parseInt(String.valueOf(op.charAt(i)));
+            }
+        }
+        return mode;
     }
 
     private boolean isSimpleOperation(int index) {
         return String.valueOf(program[index]).length() == 1;
     }
 
-    private int executeSimple(int index) {
-        int operation = program[index];
-        if (operation == INPUT) setValueByIndex(++index, input.removeFirst());
-        else if (operation == OUTPUT) output.add(getValueByIndex(++index));
-        else {
-            int arg1 = getValueByIndex(++index);
-            int arg2 = getValueByIndex(++index);
-            if (isJumpOperation(operation)) {
-                if (operation == JUMP_IF_TRUE && arg1 != 0) index = arg2 - 1;
-                if (operation == JUMP_IF_FALSE && arg1 == 0) index = arg2 - 1;
-            } else {
-                int result = getResult(operation, arg1, arg2);
-                setValueByIndex(++index, result);
-            }
-        }
-        return index;
-    }
-
-    private int executeComplex(int index) {
-        String op = String.valueOf(program[index]);
-        int operation = Integer.parseInt(op.substring(op.length() - 2));
-        int[] mode = new int[]{0, 0, 0};
-
-        for (int i = op.length() - 3, j = 0; i >= 0; i--, j++) {
-            mode[j] = Integer.parseInt(String.valueOf(op.charAt(i)));
-        }
-        if (operation == OUTPUT) {
-            output.add(getValueByIndex(++index, mode[0]));
-        } else {
-            int arg1 = getValueByIndex(++index, mode[0]);
-            int arg2 = getValueByIndex(++index, mode[1]);
-            if (isJumpOperation(operation)) {
-                if (operation == JUMP_IF_TRUE && arg1 != 0) index = arg2 - 1;
-                if (operation == JUMP_IF_FALSE && arg1 == 0) index = arg2 - 1;
-            } else {
-                int result = getResult(operation, arg1, arg2);
-                setValueByIndex(++index, result, mode[2]);
-            }
-        }
-        return index;
-    }
-
-    private int getValueByIndex(int index) {
-        return getValueByIndex(index, POSITION_MODE);
-    }
-
     private int getValueByIndex(int index, int mode) {
         if (mode == POSITION_MODE)
             return program[program[index]];
         return program[index];
-    }
-
-    private void setValueByIndex(int index, int value) {
-        setValueByIndex(index, value, POSITION_MODE);
     }
 
     private void setValueByIndex(int index, int value, int mode) {
