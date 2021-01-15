fd14900e6b56767ab6af67f0c6c4452e47684fe3
sobotkalukasz <sobotka.lukasz@gmail.com>
2021-01-15 23:52:10 +0100

    solution for AoC 2019 'Day 8: Space Image Format' Part 2

--- 2019/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SpaceImageFormat.java
+++ 2019/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SpaceImageFormat.java
@@ -3,8 +3,16 @@ package pl.lsobotka.adventofcode;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/*
+ * https://adventofcode.com/2019/day/8
+ * */
 public class SpaceImageFormat {
+
+    private final char BLACK = '0';
+    private final char WHITE = '1';
+    private final char TRANSPARENT = '2';
 
     private final int wide;
     private final int tall;
@@ -17,21 +25,34 @@ public class SpaceImageFormat {
     }
 
     private void initLayers(String encodedData) {
-        layers = new ArrayList<>();
-        int pixels = wide * tall;
-        int layerCount = encodedData.length() / pixels;
-        for (int i = 0; i < layerCount; i++) {
-            layers.add(encodedData.substring(i * pixels, i * pixels + pixels));
-        }
+        String spliterator = String.format("(?<=\\G.{%d})", wide * tall);
+        layers = new ArrayList<>(List.of(encodedData.split(spliterator)));
     }
 
     public long getLayerCode() {
-        Map<String, Long> collect = layers.stream().collect(Collectors.toMap(Function.identity(), layer -> countChar(layer, '0')));
+        Map<String, Long> collect = layers.stream().collect(Collectors.toMap(Function.identity(), layer -> countChar(layer, BLACK)));
         String layer = collect.entrySet().stream().min(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
-        return countChar(layer, '1') * countChar(layer, '2');
+        return countChar(layer, WHITE) * countChar(layer, TRANSPARENT);
     }
 
     private long countChar(CharSequence test, char c) {
         return test.chars().filter(ch -> ch == c).count();
+    }
+
+    public List<String> decodeImage() {
+        StringBuilder layerBuilder = new StringBuilder();
+
+        for (int i = 0; i < wide * tall; i++) {
+            for (String layer : layers) {
+                if (layer.charAt(i) != TRANSPARENT) {
+                    layerBuilder.append(layer.charAt(i));
+                    break;
+                }
+            }
+        }
+        String spliterator = String.format("(?<=\\G.{%d})", wide);
+        return Stream.of(layerBuilder.toString().split(spliterator))
+                .map(s -> s.replaceAll(String.valueOf(BLACK), "\s"))
+                .collect(Collectors.toList());
     }
 }
--- 2019/advent-of-code/src/test/java/pl/lsobotka/adventofcode/SpaceImageFormatTest.java
+++ 2019/advent-of-code/src/test/java/pl/lsobotka/adventofcode/SpaceImageFormatTest.java
@@ -25,4 +25,26 @@ public class SpaceImageFormatTest extends BaseTest {
         long actual = imageFormat.getLayerCode();
         assertEquals(expected, actual);
     }
+
+    private static Stream<Arguments> decodeImageFile() {
+        return Stream.of(
+                Arguments.of("SpaceImageFormat", 25, 6, List.of(
+                        "1   11111 1  1 1111 1111 ",
+                        "1   11    1  1 1    1    ",
+                        " 1 1 111  1111 111  111  ",
+                        "  1  1    1  1 1    1    ",
+                        "  1  1    1  1 1    1    ",
+                        "  1  1111 1  1 1111 1    "))
+                //YEHEF
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("decodeImageFile")
+    public void decodeImageFileTest(String fileName, int wide, int tall, List<String> expected) throws Exception {
+        List<String> input = getFileInput(fileName);
+        SpaceImageFormat imageFormat = new SpaceImageFormat(input.get(0), wide, tall);
+        List<String> actual = imageFormat.decodeImage();
+        assertEquals(expected, actual);
+    }
 }
