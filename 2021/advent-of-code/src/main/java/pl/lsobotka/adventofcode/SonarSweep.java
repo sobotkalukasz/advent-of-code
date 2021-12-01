b217adc3779e56c236ff2a6e7c2b63b678ae5da2
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2021-12-01 22:44:16 +0100

    solution for AoC 2021 'Day 1: Sonar Sweep' Star 2

--- 2021/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SonarSweep.java
+++ 2021/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SonarSweep.java
@@ -1,5 +1,6 @@
 package pl.lsobotka.adventofcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -7,19 +8,38 @@ import java.util.List;
  * */
 public class SonarSweep {
 
-    public int depthMeasurement(final List<Integer> measurements){
+    public int depthMeasurement(final List<Integer> measurements) {
 
         final int size = measurements.size();
 
         int depthIncreaseCount = 0;
-        for(int i = 1; i < size; i++ ){
+        for (int i = 1; i < size; i++) {
             final Integer first = measurements.get(i - 1);
             final Integer second = measurements.get(i);
-            if(first < second){
+            if (first < second) {
                 depthIncreaseCount++;
             }
         }
 
         return depthIncreaseCount;
     }
+
+    public int depthMeasurementSlidingWindow(final List<Integer> measurements, final int measurementsToSum) {
+
+        final int size = measurements.size();
+        final List<Integer> sumMeasurements = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            if (i + measurementsToSum <= size) {
+                int sum = 0;
+                for (int j = 0; j < measurementsToSum; j++) {
+                    sum += measurements.get(i + j);
+                }
+                sumMeasurements.add(sum);
+            }
+        }
+
+        return depthMeasurement(sumMeasurements);
+    }
 }
+
--- 2021/advent-of-code/src/test/java/pl/lsobotka/adventofcode/SonarSweepTest.java
+++ 2021/advent-of-code/src/test/java/pl/lsobotka/adventofcode/SonarSweepTest.java
@@ -1,9 +1,11 @@
 package pl.lsobotka.adventofcode;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,11 +22,36 @@ public class SonarSweepTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("starOneFile")
-    public void starOneFileTest(String fileName, int expected) throws Exception {
-        List<Integer> input = getFileInput(fileName).stream().map(Integer::valueOf).collect(Collectors.toList());
+    public void starOneFileTest(final String fileName, final int expected) throws Exception {
+        final List<Integer> input = getFileInput(fileName).stream().map(Integer::valueOf).collect(Collectors.toList());
 
-        SonarSweep sonarSweep = new SonarSweep();
-        int actual = sonarSweep.depthMeasurement(input);
+        final SonarSweep sonarSweep = new SonarSweep();
+        final int actual = sonarSweep.depthMeasurement(input);
         assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> starTwoFile() {
+        return Stream.of(
+                Arguments.of("SonarSweep", 3, 1618)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("starTwoFile")
+    public void starTwoFileTest(final String fileName, final int measurementsToSum, final int expected) throws Exception {
+        final List<Integer> input = getFileInput(fileName).stream().map(Integer::valueOf).collect(Collectors.toList());
+
+        final SonarSweep sonarSweep = new SonarSweep();
+        final int actual = sonarSweep.depthMeasurementSlidingWindow(input, measurementsToSum);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void starTwoTest() {
+        final List<Integer> input = Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
+
+        final SonarSweep sonarSweep = new SonarSweep();
+        final int actual = sonarSweep.depthMeasurementSlidingWindow(input, 3);
+        assertEquals(5, actual);
     }
 }
