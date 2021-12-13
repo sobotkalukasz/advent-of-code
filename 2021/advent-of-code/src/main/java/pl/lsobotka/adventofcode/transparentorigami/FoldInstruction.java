d1ff08c3beb2614ebd92d7341d87d9065aa2b6c5
Łukasz Sobótka <l.sobotka@firetms.com>
2021-12-13 21:44:03 +0100

    solution for AoC 2021 'Day 13: Transparent Origami'

---
+++ 2021/advent-of-code/src/main/java/pl/lsobotka/adventofcode/transparentorigami/FoldInstruction.java
@@ -0,0 +1,32 @@
+package pl.lsobotka.adventofcode.transparentorigami;
+
+public class FoldInstruction {
+
+    private final static String FOLD_DELIMITER = "=";
+    private final static String COLUMN_VALUE = "x";
+
+    private final Type type;
+    private final int value;
+
+    public FoldInstruction(final String row) {
+        final String[] split = row.split(FOLD_DELIMITER);
+        type = split[0].contains(COLUMN_VALUE) ? Type.COLUMN : Type.ROW;
+        value = Integer.parseInt(split[1]);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static boolean isFoldInstruction(final String row) {
+        return row.contains(FOLD_DELIMITER);
+    }
+
+    enum Type {
+        ROW, COLUMN
+    }
+}
