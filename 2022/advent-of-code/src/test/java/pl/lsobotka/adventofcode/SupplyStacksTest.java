c807d52e79c2e46b81874e3a80ba5d1499b5a67b
Łukasz Sobótka <l.sobotka@firetms.com>
2022-12-05 18:57:00 +0100

    solution for AoC 2022 'Day 5: Supply Stacks' - part 1

---
+++ 2022/advent-of-code/src/test/java/pl/lsobotka/adventofcode/SupplyStacksTest.java
@@ -0,0 +1,48 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SupplyStacksTest extends BaseTest {
+
+    private static Stream<Arguments> simpleFirstStarExample() {
+        return Stream.of(Arguments.of(List.of( //
+                "    [D]    ", //
+                "[N] [C]    ", //
+                "[Z] [M] [P]", //
+                " 1   2   3 ", //
+                "", //
+                "move 1 from 2 to 1", //
+                "move 3 from 1 to 3", //
+                "move 2 from 2 to 1", //
+                "move 1 from 1 to 2"), "CMZ"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("simpleFirstStarExample")
+    public void simpleFirstStarExampleTest(final List<String> input, final String expected) {
+        final SupplyStacks supplyStacks = new SupplyStacks(input);
+        final String actual = supplyStacks.applyOperationsAndGetFirstCargos();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> firstStarFile() {
+        return Stream.of(Arguments.of("SupplyStacks", "SHMSDGZVC"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStarFile")
+    public void firstStarTest(final String fileName, final String expected) throws Exception {
+        final List<String> input = getFileInput(fileName);
+        final SupplyStacks supplyStacks = new SupplyStacks(input);
+        final String actual = supplyStacks.applyOperationsAndGetFirstCargos();
+        assertEquals(expected, actual);
+    }
+
+}
