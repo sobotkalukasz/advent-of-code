78512826d5b75ea9d6ca04bcb3de05bdafb94c9f
Łukasz Sobótka <l.sobotka@firetms.com>
2022-12-06 20:18:45 +0100

    solution for AoC 2022 'Day 6: Tuning Trouble '

---
+++ 2022/advent-of-code/src/test/java/pl/lsobotka/adventofcode/TuningTroubleTest.java
@@ -0,0 +1,70 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TuningTroubleTest extends BaseTest {
+
+    private static Stream<Arguments> simpleFirstStarExample() {
+        return Stream.of(Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7), //
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 5), //
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 6), //
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10), //
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11));
+    }
+
+    @ParameterizedTest
+    @MethodSource("simpleFirstStarExample")
+    public void simpleFirstStarExampleTest(final String input, final int expected) {
+        final TuningTrouble tuningTrouble = new TuningTrouble(input);
+        final int actual = tuningTrouble.determineStartOfPacketIndex();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> simpleSecondStarExample() {
+        return Stream.of(Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19), //
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 23), //
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 23), //
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29), //
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26));
+    }
+
+    @ParameterizedTest
+    @MethodSource("simpleSecondStarExample")
+    public void simpleSecondStarExampleTest(final String input, final int expected) {
+        final TuningTrouble tuningTrouble = new TuningTrouble(input);
+        final int actual = tuningTrouble.determineStartOfMessageIndex();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> firstStarFile() {
+        return Stream.of(Arguments.of("TuningTrouble", 1109));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStarFile")
+    public void firstStarTest(final String fileName, final int expected) throws Exception {
+        final String input = String.join("", getFileInput(fileName));
+        final TuningTrouble tuningTrouble = new TuningTrouble(input);
+        final int actual = tuningTrouble.determineStartOfPacketIndex();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStarFile() {
+        return Stream.of(Arguments.of("TuningTrouble", 3965));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStarFile")
+    public void secondStarTest(final String fileName, final int expected) throws Exception {
+        final String input = String.join("", getFileInput(fileName));
+        final TuningTrouble tuningTrouble = new TuningTrouble(input);
+        final int actual = tuningTrouble.determineStartOfMessageIndex();
+        assertEquals(expected, actual);
+    }
+}
