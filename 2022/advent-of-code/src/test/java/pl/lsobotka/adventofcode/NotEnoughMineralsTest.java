e82aa3e77e295d59da22a4ce4d0ff85bd0ee8be3
Łukasz Sobótka <l.sobotka@firetms.com>
2022-12-19 23:11:03 +0100

    solution for AoC 2022 'Day 19: Not Enough Minerals'

---
+++ 2022/advent-of-code/src/test/java/pl/lsobotka/adventofcode/NotEnoughMineralsTest.java
@@ -0,0 +1,42 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotEnoughMineralsTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of(Arguments.of("NotEnoughMinerals_simple", 33), //
+                Arguments.of("NotEnoughMinerals", 1382));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    public void firstStarTest(final String fileName, final long expected) throws Exception {
+        final List<String> input = getFileInput(fileName);
+        final NotEnoughMinerals minerals = new NotEnoughMinerals(input);
+        final int actual = minerals.qualityLevel();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("NotEnoughMinerals_simple", 2, 3472), //
+                Arguments.of("NotEnoughMinerals", 3, 31740));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    public void secondStarTest(final String fileName, final int amount, final long expected) throws Exception {
+        final List<String> input = getFileInput(fileName);
+        final NotEnoughMinerals minerals = new NotEnoughMinerals(input);
+        final int actual = minerals.qualityLevelOfFirst(amount);
+        assertEquals(expected, actual);
+    }
+
+}
