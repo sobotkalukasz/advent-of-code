4b30529ddbadb25dcdc87eb4cd5149f7cc7a1499
Łukasz Sobótka <l.sobotka@firetms.com>
2022-12-24 16:48:18 +0100

    solution for AoC 2022 'Day 24: Blizzard Basin'

---
+++ 2022/advent-of-code/src/test/java/pl/lsobotka/adventofcode/BlizzardBasinTest.java
@@ -0,0 +1,42 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlizzardBasinTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of(Arguments.of("BlizzardBasin_simple", 18), //
+                Arguments.of("BlizzardBasin", 314));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    public void firstStarTest(final String fileName, final long expected) throws Exception {
+        final List<String> input = getFileInput(fileName);
+        final BlizzardBasin basin = new BlizzardBasin(input);
+        final int actual = basin.oneWay();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("BlizzardBasin_simple", 54), //
+                Arguments.of("BlizzardBasin", 896));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    public void secondStarTest(final String fileName, final long expected) throws Exception {
+        final List<String> input = getFileInput(fileName);
+        final BlizzardBasin basin = new BlizzardBasin(input);
+        final int actual = basin.goOnceAgain();
+        assertEquals(expected, actual);
+    }
+
+}
