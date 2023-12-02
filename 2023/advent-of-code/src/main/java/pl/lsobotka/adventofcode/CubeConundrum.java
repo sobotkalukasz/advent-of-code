d7a94a98bfb3ad5cc1e2075ef9f62ce47c82b89e
Łukasz Sobótka <l.sobotka@firetms.com>
2023-12-02 11:55:02 +0100

    solution for AoC 2023 'Day 2: CubeConundrum'

---
+++ 2023/advent-of-code/src/main/java/pl/lsobotka/adventofcode/CubeConundrum.java
@@ -0,0 +1,78 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * https://adventofcode.com/2023/day/2
+ * */
+public class CubeConundrum {
+
+    private final List<Game> games;
+
+    public CubeConundrum(List<String> input) {
+        this.games = input.stream().map(Game::of).toList();
+    }
+
+    int sumIdsOfPossibleGames() {
+        final Map<Colour, Integer> minValues = Map.of(Colour.RED, 12, Colour.GREEN, 13, Colour.BLUE, 14);
+        return games.stream().filter(g -> g.isPossible(minValues)).map(Game::id).mapToInt(Integer::intValue).sum();
+    }
+
+    int sumThePowerOfGames() {
+        return games.stream().map(Game::getPowerOfGame).mapToInt(Integer::intValue).sum();
+    }
+
+    enum Colour {
+        BLUE(Pattern.compile("([0-9]+) blue")), RED(Pattern.compile("([0-9]+) red")), GREEN(
+                Pattern.compile("([0-9]+) green"));
+
+        final Pattern pattern;
+
+        Colour(Pattern pattern) {
+            this.pattern = pattern;
+        }
+    }
+
+    record Game(int id, Map<Colour, Integer> cubes) {
+
+        private static final Pattern gameIdPattern = Pattern.compile("Game ([0-9]+):");
+
+        static Game of(final String gameRow) {
+            final Map<Colour, Integer> valueMap = new HashMap<>(Map.of(Colour.RED, 0, Colour.GREEN, 0, Colour.BLUE, 0));
+            int id = 0;
+
+            final Matcher idMatcher = gameIdPattern.matcher(gameRow);
+            if (idMatcher.find()) {
+                id = Integer.parseInt(idMatcher.group(1));
+            }
+
+            for (Colour colour : Colour.values()) {
+                final Matcher matcher = colour.pattern.matcher(gameRow);
+                while (matcher.find()) {
+                    final int actual = Integer.parseInt(matcher.group(1));
+                    valueMap.computeIfPresent(colour, (key, val) -> val > actual ? val : actual);
+                }
+            }
+
+            return new Game(id, valueMap);
+        }
+
+        boolean isPossible(final Map<Colour, Integer> forValues) {
+            for (Colour colour : forValues.keySet()) {
+                if (cubes.get(colour).compareTo(forValues.get(colour)) > 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        int getPowerOfGame() {
+            return cubes.values().stream().reduce((a, b) -> a * b).orElse(0);
+        }
+
+    }
+}
---
+++ 2023/advent-of-code/src/test/java/pl/lsobotka/adventofcode/CubeConundrumTest.java
@@ -0,0 +1,42 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CubeConundrumTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of(Arguments.of("CubeConundrum_example", 8), //
+                Arguments.of("CubeConundrum", 2061));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    public void firstStarTest(final String fileName, final int expected) {
+        final List<String> input = getFileInput(fileName);
+        final CubeConundrum cubeConundrum = new CubeConundrum(input);
+        final int actual = cubeConundrum.sumIdsOfPossibleGames();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("CubeConundrum_example", 2286), //
+                Arguments.of("CubeConundrum", 72596));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    public void secondStarTest(final String fileName, final int expected) {
+        final List<String> input = getFileInput(fileName);
+        final CubeConundrum cubeConundrum = new CubeConundrum(input);
+        final int actual = cubeConundrum.sumThePowerOfGames();
+        assertEquals(expected, actual);
+    }
+
+}
