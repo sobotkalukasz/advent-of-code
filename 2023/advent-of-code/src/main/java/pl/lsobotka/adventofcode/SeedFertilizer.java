b7f68b607b079084522ad0d5b12ae70186714a40
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2023-12-06 21:42:45 +0100

    solution for AoC 2023 Day 5: 'SeedFertilizer' - replaced bruteforce solution for 2nd star

--- 2023/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SeedFertilizer.java
+++ 2023/advent-of-code/src/main/java/pl/lsobotka/adventofcode/SeedFertilizer.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,52 +28,54 @@ public class SeedFertilizer {
     private final List<MappingHolder> mappingHolders;
 
     public SeedFertilizer(final List<String> input) {
-
         final String inputAsSingleRow = String.join(" ", input);
 
-        this.seedNumbers = getSeeds(inputAsSingleRow);
+        this.seedNumbers = parseSeedNumbers(inputAsSingleRow);
         this.mappingHolders = MAPPING_NAMES.stream().map(name -> MappingHolder.from(name, inputAsSingleRow)).toList();
-
     }
 
     long findLowestLocation() {
-        return seedNumbers.stream().map(s -> {
-            long actual = s;
-            for (MappingHolder mappingHolder : mappingHolders) {
-                actual = mappingHolder.map(actual);
-            }
-            return actual;
-        }).reduce(Long.MAX_VALUE, Long::min);
+        return seedNumbers.stream().map(this::evaluateSeed).reduce(Long.MAX_VALUE, Long::min);
     }
 
-    /*
-    * A reverse brute force solution that I'm not satisfied with
-    * */
+    private long evaluateSeed(final long seed) {
+        long actual = seed;
+        for (MappingHolder mappingHolder : mappingHolders) {
+            actual = mappingHolder.map(actual);
+        }
+        return actual;
+    }
+
     long findLowestLocationForSeedRange() {
+        final List<SeedRange> seeds = SeedRange.ofRanges(seedNumbers);
 
-        List<Seed> seeds = new ArrayList<>();
-        for (int i = 0; i < seedNumbers.size() - 1; i = i + 2) {
-            seeds.add(new Seed(seedNumbers.get(i), seedNumbers.get(i + 1)));
+        final List<SeedRange> evaluatedRanges = new ArrayList<>();
+        for (SeedRange seed : seeds) {
+            evaluatedRanges.addAll(evaluateSeedRange(seed));
         }
 
-        final List<MappingHolder> reversedMappings = new ArrayList<>(mappingHolders);
-        Collections.reverse(reversedMappings);
-
-        long location = 0;
-        for (; location < Integer.MAX_VALUE; location++) {
-            long actual = location;
-            for (MappingHolder mappingHolder : reversedMappings) {
-                actual = mappingHolder.reverse(actual);
-            }
-            final long possibleSeed = actual;
-            if (seeds.stream().anyMatch(s -> s.exists(possibleSeed))) {
-                break;
-            }
-        }
-        return location;
+        return evaluatedRanges.stream().map(SeedRange::start).reduce(Long.MAX_VALUE, Long::min);
     }
 
-    private List<Long> getSeeds(final String inputAsSingleRow) {
+    private Set<SeedRange> evaluateSeedRange(final SeedRange seed) {
+        final Set<SeedRange> nextIterationRanges = new HashSet<>(Collections.singletonList(seed));
+        for (MappingHolder mappingHolder : mappingHolders) {
+            final List<SeedRange> mappedRanges = new ArrayList<>();
+            for (Mapping mapping : mappingHolder.mappings) {
+                final List<SeedRange> leftToRemap = new ArrayList<>();
+                for (SeedRange seedToMap : nextIterationRanges) {
+                    mappedRanges.addAll(mapping.remapRange(seedToMap));
+                    leftToRemap.addAll(mapping.getRangesLeftToRemap(seedToMap));
+                }
+                nextIterationRanges.clear();
+                nextIterationRanges.addAll(leftToRemap);
+            }
+            nextIterationRanges.addAll(mappedRanges);
+        }
+        return nextIterationRanges;
+    }
+
+    private List<Long> parseSeedNumbers(final String inputAsSingleRow) {
 
         final Matcher seedMatcher = SEED_PATTERN.matcher(inputAsSingleRow);
         if (seedMatcher.find()) {
@@ -88,14 +92,6 @@ public class SeedFertilizer {
     record MappingHolder(String name, List<Mapping> mappings) {
         long map(long toMap) {
             return mappings.stream().filter(m -> m.canMap(toMap)).map(m -> m.map(toMap)).findFirst().orElse(toMap);
-        }
-
-        long reverse(long toMap) {
-            return mappings.stream()
-                    .filter(m -> m.canReverse(toMap))
-                    .map(m -> m.reverse(toMap))
-                    .findFirst()
-                    .orElse(toMap);
         }
 
         static MappingHolder from(final String name, final String input) {
@@ -130,35 +126,77 @@ public class SeedFertilizer {
 
     }
 
-    record Mapping(long destination, long source, long range) {
+    record Mapping(long destinationStart, long destinationEnd, long sourceStart, long sourceEnd) {
         boolean canMap(long toMap) {
-            return toMap >= source && toMap <= source + range;
-        }
-
-        boolean canReverse(long toMap) {
-            return toMap >= destination && toMap <= destination + range;
+            return toMap >= sourceStart && toMap <= sourceEnd;
         }
 
         long map(long toMap) {
-            return toMap - source + destination;
+            return toMap - sourceStart + destinationStart;
         }
 
-        long reverse(long toMap) {
-            return toMap - destination + source;
+        List<SeedRange> remapRange(final SeedRange seed) {
+            final List<SeedRange> newSeeds = new ArrayList<>();
+
+            if (seed.end < sourceStart || sourceEnd < seed.start) {
+                // do nothing - nothing to remap
+            } else if (seed.start >= sourceStart && seed.end <= sourceEnd) {
+                final long offSet = seed.start - sourceStart;
+                final long range = seed.end - seed.start;
+                newSeeds.add(new SeedRange(destinationStart + offSet, destinationStart + offSet + range));
+            } else if (seed.start >= sourceStart) {
+                newSeeds.add(new SeedRange(destinationEnd - (sourceEnd - seed.start), destinationEnd));
+            } else if (seed.end <= sourceEnd) {
+                newSeeds.add(new SeedRange(destinationStart, destinationEnd - (sourceEnd - seed.end)));
+            } else {
+                newSeeds.add(new SeedRange(destinationStart, destinationEnd));
+            }
+
+            return newSeeds;
+        }
+
+        List<SeedRange> getRangesLeftToRemap(final SeedRange seed) {
+            final List<SeedRange> newSeeds = new ArrayList<>();
+
+            if (seed.end < sourceStart || sourceEnd < seed.start) {
+                newSeeds.add(seed);
+            } else if (seed.start >= sourceStart && seed.end <= sourceEnd) {
+                // do nothing - fully evaluated
+            } else if (seed.start >= sourceStart) {
+                newSeeds.add(new SeedRange(sourceEnd + 1, seed.end));
+            } else if (seed.end <= sourceEnd) {
+                newSeeds.add(new SeedRange(seed.start, sourceStart - 1));
+            } else {
+                newSeeds.add(new SeedRange(seed.start, sourceStart - 1));
+                newSeeds.add(new SeedRange(sourceEnd + 1, seed.end));
+            }
+
+            return newSeeds;
         }
 
         static Mapping from(final List<Long> numbers) {
             if (numbers.size() == 3) {
-                return new Mapping(numbers.get(0), numbers.get(1), numbers.get(2));
+                final long destinationStart = numbers.get(0);
+                final long sourceStart = numbers.get(1);
+                final long range = numbers.get(2);
+                return new Mapping(destinationStart, destinationStart + range, sourceStart, sourceStart + range);
             }
             throw new IllegalArgumentException("Unknown mapping numbers: " + numbers);
         }
     }
 
-    record Seed(long from, long range) {
-        boolean exists(long possibleSeed) {
-            return possibleSeed >= from && possibleSeed <= from + range;
+    record SeedRange(long start, long end) {
+
+        static List<SeedRange> ofRanges(List<Long> ranges) {
+            final List<SeedRange> seeds = new ArrayList<>();
+            for (int i = 0; i < ranges.size() - 1; i = i + 2) {
+                final long from = ranges.get(i);
+                final long to = from + ranges.get(i + 1) - 1;
+                seeds.add(new SeedRange(from, to));
+            }
+            return seeds;
         }
+
     }
 
 }
