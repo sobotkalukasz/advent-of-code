5576253331083c97f07f670f37fb5c2f7213403a
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2023-12-14 17:30:31 +0100

    solution for AoC 2023 Day 14: 'Parabolic Reflector Dish'

---
+++ 2023/advent-of-code/src/test/java/pl/lsobotka/adventofcode/ParabolicReflectorDishTest.java
@@ -0,0 +1,42 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParabolicReflectorDishTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of(Arguments.of("ParabolicReflectorDish_example", 136), //
+                Arguments.of("ParabolicReflectorDish", 108_759));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    void firstStarTest(final String fileName, final long expected) {
+        final List<String> input = getFileInput(fileName);
+        final ParabolicReflectorDish parabolicReflectorDish = new ParabolicReflectorDish(input);
+        final long actual = parabolicReflectorDish.calculateLoad();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("ParabolicReflectorDish_example", 1_000_000_000, 64), //
+                Arguments.of("ParabolicReflectorDish", 1_000_000_000, 89_089));
+    }
+ 
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    void secondStarTest(final String fileName, final long cycle, final long expected) {
+        final List<String> input = getFileInput(fileName);
+        final ParabolicReflectorDish parabolicReflectorDish = new ParabolicReflectorDish(input);
+        final long actual = parabolicReflectorDish.calculateLoadAfterCycles(cycle);
+        assertEquals(expected, actual);
+    }
+
+}
