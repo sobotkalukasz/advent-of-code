c70785f3298541438308ccfced66751ee3f19390
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2023-12-15 14:58:47 +0100

    solution for AoC 2023 Day 15: 'Lens Library'

---
+++ 2023/advent-of-code/src/test/java/pl/lsobotka/adventofcode/LensLibraryTest.java
@@ -0,0 +1,41 @@
+package pl.lsobotka.adventofcode;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LensLibraryTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of(Arguments.of("LensLibrary_example", 1320), //
+                Arguments.of("LensLibrary", 511_215));
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    void firstStarTest(final String fileName, final long expected) {
+        final String input = getFileInputSingleLine(fileName);
+        final LensLibrary lensLibrary = new LensLibrary(input);
+        final long actual = lensLibrary.sumOfLensHash();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("LensLibrary_example", 145), //
+                Arguments.of("LensLibrary", 236_057));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    void secondStarTest(final String fileName, final long expected) {
+        final String input = getFileInputSingleLine(fileName);
+        final LensLibrary lensLibrary = new LensLibrary(input);
+        final long actual = lensLibrary.calculateFocussingPower();
+        assertEquals(expected, actual);
+    }
+
+}
