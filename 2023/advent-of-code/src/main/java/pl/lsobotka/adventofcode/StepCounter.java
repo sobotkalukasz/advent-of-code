a4a6846e7abca27ca274ae00fff668e7498f1ee2
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2023-12-21 14:50:09 +0100

    solution for AoC 2023 Day 21: 'Step Counter' part 2

--- 2023/advent-of-code/src/main/java/pl/lsobotka/adventofcode/StepCounter.java
+++ 2023/advent-of-code/src/main/java/pl/lsobotka/adventofcode/StepCounter.java
@@ -2,7 +2,11 @@ package pl.lsobotka.adventofcode;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import pl.lsobotka.adventofcode.utils.Coord;
 
@@ -12,15 +16,13 @@ import pl.lsobotka.adventofcode.utils.Coord;
 public class StepCounter {
 
     private final Set<Coord> walls;
-    private final Coord max;
+    private final int max;
     private Coord start;
 
     public StepCounter(final List<String> input) {
         this.walls = new HashSet<>();
 
-        int maxRow = input.size() - 1;
-        int maxCol = input.get(0).length() - 1;
-        this.max = Coord.of(maxRow, maxCol);
+        max = input.size();
 
         for (int row = 0; row < input.size(); row++) {
             final String rowString = input.get(row);
@@ -48,11 +50,98 @@ public class StepCounter {
         return actualSteps.size();
     }
 
+    long howManyReachedInfinite(final long steps) {
+        final Set<Coord> actualSteps = new HashSet<>(Set.of(start));
+
+        final Map<Integer, Integer> diffs = IntStream.range(0, max)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), v -> 0));
+
+        final Map<Integer, Integer> deltaDiffs = IntStream.range(0, max)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), v -> 0));
+
+        final Map<Integer, Integer> deltaDiffsChange = IntStream.range(0, max)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), v -> 0));
+
+        int previousStepSize = 0;
+        int previosDiff = 0;
+        long size = 0;
+
+        for (int step = 1; step <= steps; step++) {
+            final int modulo = step % (max);
+
+            if (size > 0) {
+                final int change = deltaDiffsChange.get(modulo);
+                final int delta = deltaDiffs.get(modulo) + change;
+                final int diff = diffs.get((step - 1) % (max)) + delta;
+
+                size += diff;
+                diffs.put(modulo, diff);
+                deltaDiffs.put(modulo, delta);
+
+            } else {
+                final Set<Coord> nextSteps = new HashSet<>();
+                for (Coord coord : actualSteps) {
+                    nextSteps.addAll(nextInfinitePosition(coord));
+                }
+                actualSteps.clear();
+                actualSteps.addAll(nextSteps);
+
+                final int diff = actualSteps.size() - previousStepSize;
+                final int deltaDiff = diff - previosDiff;
+                previousStepSize = actualSteps.size();
+                previosDiff = diff;
+
+                diffs.put(modulo, diff);
+                final int change = deltaDiff - deltaDiffs.get(modulo);
+                deltaDiffs.put(modulo, deltaDiff);
+
+                if (deltaDiffsChange.get(modulo) != change) {
+                    deltaDiffsChange.put(modulo, change);
+                } else if (modulo == max - 1) {
+                    size = actualSteps.size();
+                }
+            }
+        }
+
+        return size > 0 ? size : actualSteps.size();
+    }
+
     private Set<Coord> nextPositions(final Coord actual) {
         final Set<Coord> direct = actual.getDirectAdjacent();
         direct.removeIf(walls::contains);
-        direct.removeIf(c -> c.row() < 0 || c.row() > max.row() || c.col() < 0 || c.col() > max.col());
+        direct.removeIf(c -> c.row() < 0 || c.row() >= max || c.col() < 0 || c.col() >= max);
         return direct;
+    }
+
+    private Set<Coord> nextInfinitePosition(final Coord actual) {
+        final Set<Coord> direct = actual.getDirectAdjacent();
+        direct.removeIf(c -> walls.contains(translate(c)));
+        return direct;
+    }
+
+    private Coord translate(final Coord c) {
+        final Coord translated;
+        if (c.row() >= 0 && c.row() < max && c.col() >= 0 && c.col() < max) {
+            translated = c;
+        } else {
+            final int row;
+            final int col;
+            if (c.row() < 0) {
+                row = max + c.row() % max;
+            } else {
+                row = c.row() % max;
+            }
+            if (c.col() < 0) {
+                col = max + c.col() % max;
+            } else {
+                col = c.col() % max;
+            }
+            translated = Coord.of(row, col);
+        }
+        return translated;
     }
 
 }
--- 2023/advent-of-code/src/test/java/pl/lsobotka/adventofcode/StepCounterTest.java
+++ 2023/advent-of-code/src/test/java/pl/lsobotka/adventofcode/StepCounterTest.java
@@ -25,4 +25,24 @@ class StepCounterTest extends BaseTest {
         assertEquals(expected, actual);
     }
 
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("StepCounter_example", 6, 16), //
+                Arguments.of("StepCounter_example", 10, 50), //
+                Arguments.of("StepCounter_example", 50, 1594),//
+                Arguments.of("StepCounter_example", 100, 6536), //
+                Arguments.of("StepCounter_example", 500, 167004), //
+                Arguments.of("StepCounter_example", 1000, 668697), //
+                Arguments.of("StepCounter_example", 5000, 16733044), //
+                Arguments.of("StepCounter", 26501365, 616_583_483_179_597L));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    void secondStarTest(final String fileName, final long steps, final long expected) {
+        final List<String> input = getFileInput(fileName);
+        final StepCounter stepCounter = new StepCounter(input);
+        final long actual = stepCounter.howManyReachedInfinite(steps);
+        assertEquals(expected, actual);
+    }
+
 }
