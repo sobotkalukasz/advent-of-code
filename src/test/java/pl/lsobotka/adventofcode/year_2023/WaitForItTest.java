da919bf0556e5127ec3183d3d71cb7b359907edc
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2023-12-22 10:26:55 +0100

    Clean up - removed public modifier from tests

--- src/test/java/pl/lsobotka/adventofcode/year_2023/WaitForItTest.java
+++ src/test/java/pl/lsobotka/adventofcode/year_2023/WaitForItTest.java
@@ -20,7 +20,7 @@ class WaitForItTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("firstStar")
-    public void firstStarTest(final String fileName, final long expected) {
+    void firstStarTest(final String fileName, final long expected) {
         final List<String> input = getFileInput(fileName);
         final WaitForIt waitForIt = WaitForIt.ofRaces(input);
         final long actual = waitForIt.countWaysToBeatDistanceRecord();
@@ -34,7 +34,7 @@ class WaitForItTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("secondStar")
-    public void secondStarTest(final String fileName, final long expected) {
+    void secondStarTest(final String fileName, final long expected) {
         final List<String> input = getFileInput(fileName);
         final WaitForIt waitForIt = WaitForIt.ofSingleRace(input);
         final long actual = waitForIt.countWaysToBeatDistanceRecord();
