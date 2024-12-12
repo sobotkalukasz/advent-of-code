465748fb271bc9125e566340371d562ff285f004
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2024-12-12 21:03:18 +0100

    solution fo AoC 2024 Day 12: 'Garden Groups' part 2

--- src/test/java/pl/lsobotka/adventofcode/year_2024/GardenGroups.java
+++ src/test/java/pl/lsobotka/adventofcode/year_2024/GardenGroups.java
@@ -1,21 +1,22 @@
 package pl.lsobotka.adventofcode.year_2024;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.function.ToIntFunction;
 
 import pl.lsobotka.adventofcode.utils.Coord;
 
 public class GardenGroups {
 
-    Map<Character, Set<Coord>> gardenMap;
+    private final Map<Character, Set<Coord>> gardenMap;
 
     GardenGroups(final List<String> lines) {
-
         this.gardenMap = new HashMap<>();
 
         for (int row = 0; row < lines.size(); row++) {
@@ -26,13 +27,17 @@ public class GardenGroups {
         }
     }
 
-    long fencePrice() {
-        final Map<Character, Set<Set<Coord>>> gardenSubregions = createSubregions(gardenMap);
+    int fencePrice() {
+        return calculatePrice(this::subregionFencePrice);
+    }
 
-        return gardenSubregions.values()
-                .stream()
-                .mapToLong(set -> set.stream().mapToLong(this::subregionFencePrice).sum())
-                .sum();
+    int fencePriceBySide() {
+        return calculatePrice(this::subregionFencePriceBySide);
+    }
+
+    private int calculatePrice(final ToIntFunction<Set<Coord>> strategy) {
+        final Map<Character, Set<Set<Coord>>> gardenSubregions = createSubregions(gardenMap);
+        return gardenSubregions.values().stream().mapToInt(set -> set.stream().mapToInt(strategy).sum()).sum();
     }
 
     private Map<Character, Set<Set<Coord>>> createSubregions(final Map<Character, Set<Coord>> garden) {
@@ -45,36 +50,86 @@ public class GardenGroups {
 
             for (final Coord coord : coords) {
                 if (!visited.contains(coord)) {
-                    final Set<Coord> adjacent = new HashSet<>();
+                    final Set<Coord> subregion = new HashSet<>();
                     final Queue<Coord> stack = new ArrayDeque<>();
                     stack.add(coord);
                     visited.add(coord);
 
                     while (!stack.isEmpty()) {
                         final Coord current = stack.poll();
-                        adjacent.add(current);
+                        subregion.add(current);
                         for (Coord neighbor : current.getDirectAdjacent()) {
                             if (coords.contains(neighbor) && visited.add(neighbor)) {
                                 stack.add(neighbor);
                             }
                         }
                     }
-                    gardenSubregions.computeIfAbsent(key, v -> new HashSet<>()).add(adjacent);
+                    gardenSubregions.computeIfAbsent(key, v -> new HashSet<>()).add(subregion);
                 }
             }
-
         }
 
         return gardenSubregions;
     }
 
-    private long subregionFencePrice(final Set<Coord> subregion) {
-        final int fenceCount = subregion.stream().mapToInt(c -> {
-            final Set<Coord> directAdjacent = c.getDirectAdjacent();
-            directAdjacent.removeIf(subregion::contains);
-            return directAdjacent.size();
-        }).sum();
-
-        return (long) fenceCount * subregion.size();
+    private int subregionFencePrice(final Set<Coord> subregion) {
+        final int fenceCount = subregion.stream()
+                .mapToInt(c -> (int) c.getDirectAdjacent()
+                        .stream()
+                        .filter(neighbor -> !subregion.contains(neighbor))
+                        .count())
+                .sum();
+        return fenceCount * subregion.size();
     }
+
+    private int subregionFencePriceBySide(final Set<Coord> subregion) {
+        final int sides = calculateVertices(subregion);
+        return sides * subregion.size();
+
+    }
+
+    public int calculateVertices(final Set<Coord> centers) {
+        final Map<CoordVertex, Integer> vertexCount = new HashMap<>();
+        for (Coord center : centers) {
+            for (CoordVertex vertex : CoordVertex.toVertex(center)) {
+                vertexCount.put(vertex, vertexCount.getOrDefault(vertex, 0) + 1);
+            }
+        }
+        int result = 0;
+        for (var entry : vertexCount.entrySet()) {
+            if (entry.getValue() == 1 || entry.getValue() == 3) {
+                result++;
+            } else if (entry.getValue() == 2 && isCornerTouch(centers, entry.getKey())) {
+                result += 2;
+            }
+        }
+        return result;
+    }
+
+    private boolean isCornerTouch(Set<Coord> centers, CoordVertex vertex) {
+        final List<Coord> coords = CoordVertex.toCenter(vertex);
+        coords.removeIf(centers::contains);
+        return !coords.getFirst().getDirectAdjacent().contains(coords.get(1));
+    }
+
+    record CoordVertex(double row, double col) {
+        static Set<CoordVertex> toVertex(final Coord center) {
+            final Set<CoordVertex> vertex = new HashSet<>();
+            vertex.add(new CoordVertex(center.row() - 0.5, center.col() - 0.5));
+            vertex.add(new CoordVertex(center.row() + 0.5, center.col() - 0.5));
+            vertex.add(new CoordVertex(center.row() + 0.5, center.col() + 0.5));
+            vertex.add(new CoordVertex(center.row() - 0.5, center.col() + 0.5));
+            return vertex;
+        }
+
+        static List<Coord> toCenter(final CoordVertex vertex) {
+            List<Coord> center = new ArrayList<>();
+            center.add(new Coord((int) (vertex.row() - 0.5), (int) (vertex.col() - 0.5)));
+            center.add(new Coord((int) (vertex.row() + 0.5), (int) (vertex.col() - 0.5)));
+            center.add(new Coord((int) (vertex.row() + 0.5), (int) (vertex.col() + 0.5)));
+            center.add(new Coord((int) (vertex.row() - 0.5), (int) (vertex.col() + 0.5)));
+            return center;
+        }
+    }
+
 }
--- src/test/java/pl/lsobotka/adventofcode/year_2024/GardenGroupsTest.java
+++ src/test/java/pl/lsobotka/adventofcode/year_2024/GardenGroupsTest.java
@@ -12,6 +12,7 @@ import pl.lsobotka.adventofcode.BaseTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GardenGroupsTest extends BaseTest {
+
     private static Stream<Arguments> firstStar() {
         return Stream.of(Arguments.of("2024/GardenGroups_example_1", 140), //
                 Arguments.of("2024/GardenGroups_example_2", 772), //
@@ -25,6 +26,24 @@ class GardenGroupsTest extends BaseTest {
         final List<String> input = getFileInput(fileName);
         final GardenGroups gardenGroups = new GardenGroups(input);
         final long actual = gardenGroups.fencePrice();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("2024/GardenGroups_example_1", 80), //
+                Arguments.of("2024/GardenGroups_example_2", 436), //
+                Arguments.of("2024/GardenGroups_example_3", 1206), //
+                Arguments.of("2024/GardenGroups_example_4", 236), //
+                Arguments.of("2024/GardenGroups_example_5", 368), //
+                Arguments.of("2024/GardenGroups", 821428));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    void secondStarTest(final String fileName, final long expected) {
+        final List<String> input = getFileInput(fileName);
+        final GardenGroups gardenGroups = new GardenGroups(input);
+        final long actual = gardenGroups.fencePriceBySide();
         assertEquals(expected, actual);
     }
 
