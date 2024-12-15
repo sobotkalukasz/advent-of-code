5477f749201ebe7c85628a39dbba7421fe84f2f4
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2024-12-15 15:56:52 +0100

    solution fo AoC 2024 Day 15: 'Warehouse Woes' part 2

--- src/main/java/pl/lsobotka/adventofcode/year_2024/WarehouseWoes.java
+++ src/main/java/pl/lsobotka/adventofcode/year_2024/WarehouseWoes.java
@@ -1,10 +1,14 @@
 package pl.lsobotka.adventofcode.year_2024;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import pl.lsobotka.adventofcode.utils.Coord;
 import pl.lsobotka.adventofcode.utils.Dir;
@@ -15,101 +19,135 @@ import pl.lsobotka.adventofcode.utils.Dir;
 public class WarehouseWoes {
     private final Warehouse warehouse;
 
-    WarehouseWoes(final List<String> input) {
-        this.warehouse = Warehouse.from(input);
+    private WarehouseWoes(final Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    static WarehouseWoes small(final List<String> input) {
+        return new WarehouseWoes(Warehouse.small(input));
+    }
+
+    static WarehouseWoes big(final List<String> input) {
+        return new WarehouseWoes(Warehouse.big(input));
     }
 
     long sumOfBoxesCoordinate() {
-        warehouse.applyMoves();
-        return warehouse.sumBoxesCoordinates();
+        final Warehouse actual = warehouse.applyMoves();
+        return actual.sumBoxesCoordinates();
     }
 
-    record Warehouse(Set<Coord> walls, Set<Coord> boxes, Robot robot) {
-        static Warehouse from(final List<String> input) {
+    record Warehouse(Set<Coord> walls, Set<Box> boxes, Robot robot) {
+        static Warehouse small(final List<String> input) {
+            return createWarehouse(input, 1);
+        }
+
+        static Warehouse big(final List<String> input) {
+            return createWarehouse(input, 2);
+        }
+
+        private static Warehouse createWarehouse(final List<String> input, final int scalingFactor) {
             final Set<Coord> walls = new HashSet<>();
-            final Set<Coord> boxes = new HashSet<>();
+            final Set<Box> boxes = new HashSet<>();
             final Queue<Dir> moves = new ArrayDeque<>();
             Coord robotPos = null;
 
             for (int row = 0; row < input.size(); row++) {
                 final String rowS = input.get(row);
                 for (int col = 0; col < rowS.length(); col++) {
+                    final int scaledCol = col * scalingFactor;
                     switch (rowS.charAt(col)) {
-                    case '#' -> walls.add(Coord.of(row, col));
-                    case 'O' -> boxes.add(Coord.of(row, col));
-                    case '@' -> robotPos = Coord.of(row, col);
+                    case '#' -> {
+                        for (int i = 0; i < scalingFactor; i++) {
+                            walls.add(Coord.of(row, scaledCol + i));
+                        }
+                    }
+                    case 'O' -> {
+                        final Box box = Box.from(Coord.of(row, scaledCol), scalingFactor);
+                        boxes.add(box);
+                    }
+                    case '@' -> robotPos = Coord.of(row, scaledCol);
                     case '<' -> moves.add(Dir.LEFT);
                     case '>' -> moves.add(Dir.RIGHT);
                     case '^' -> moves.add(Dir.UP);
                     case 'v' -> moves.add(Dir.DOWN);
-                    default -> { //do nothing
+                    default -> { // Do nothing
                     }
                     }
-
                 }
             }
 
             return new Warehouse(walls, boxes, new Robot(robotPos, moves));
         }
 
-        void applyMoves() {
-            Robot current = robot;
+        Warehouse applyMoves() {
+            final Map<Coord, Box> boxCoords = computeBoxCoords();
+            Robot current = robot.copy();
 
             while (current.moveAvailable()) {
                 final Dir dir = current.next();
-                final Set<Coord> boxesToMove = new HashSet<>();
-                Coord next = current.pos().getAdjacent(dir);
-                Coord wall = null;
+                final Set<Box> boxesToMove = new HashSet<>();
+                Set<Coord> next = new HashSet<>();
+                next.add(current.pos().getAdjacent(dir));
+                boolean wall = false;
 
-                while (next != null) {
-                    if (boxes.contains(next)) {
-                        boxesToMove.add(next);
-                        next = next.getAdjacent(dir);
-                    } else if (walls.contains(next)) {
-                        wall = next;
-                        next = null;
+                while (!next.isEmpty()) {
+                    if (next.stream().anyMatch(walls::contains)) {
+                        wall = true;
+                        break;
+                    }
+                    final Set<Box> stepBoxes = next.stream()
+                            .filter(boxCoords::containsKey)
+                            .map(boxCoords::get)
+                            .collect(Collectors.toSet());
+
+                    if (!stepBoxes.isEmpty()) {
+                        boxesToMove.addAll(stepBoxes);
+                        next = new HashSet<>();
+                        for (Box b : stepBoxes) {
+                            switch (dir) {
+                            case UP, DOWN -> {
+                                next.add(b.left().getAdjacent(dir));
+                                next.add(b.right().getAdjacent(dir));
+                            }
+                            case LEFT -> next.add(b.left().getAdjacent(dir));
+                            case RIGHT -> next.add(b.right().getAdjacent(dir));
+                            }
+                        }
                     } else {
-                        next = null;
+                        next = Collections.emptySet();
                     }
                 }
 
-                if (wall == null) {
-                    boxes.removeIf(boxesToMove::contains);
-                    boxesToMove.forEach(b -> boxes.add(b.getAdjacent(dir)));
-                    current = new Robot(current.pos().getAdjacent(dir), current.moves());
+                if (!wall) {
+                    boxesToMove.forEach(b -> {
+                        boxCoords.remove(b.left());
+                        boxCoords.remove(b.right());
+                    });
+
+                    boxesToMove.forEach(b -> {
+                        final Box moved = b.move(dir);
+                        boxCoords.put(moved.left(), moved);
+                        boxCoords.put(moved.right(), moved);
+                    });
+
+                    current = current.move(dir);
                 }
-                //printWarehouseMap(dir, current);
             }
 
-            new Warehouse(walls, boxes, current);
+            return new Warehouse(walls, new HashSet<>(boxCoords.values()), current);
         }
 
         long sumBoxesCoordinates() {
-            return boxes.stream().mapToLong(c -> c.row() * 100L + c.col()).sum();
+            return boxes.stream().mapToLong(Box::gpsPosition).sum();
         }
 
-        void printWarehouseMap(final Dir dir, final Robot robot) {
-            final int maxCol = walls.stream().mapToInt(Coord::col).max().orElseGet(() -> 0);
-            final int maxRow = walls.stream().mapToInt(Coord::row).max().orElseGet(() -> 0);
-            final StringBuilder map = new StringBuilder();
-            map.append("Warehouse after move: ").append(dir.name()).append("\n");
-            for (int row = 0; row <= maxRow; row++) {
-                for (int col = 0; col <= maxCol; col++) {
-                    final Coord c = Coord.of(row, col);
-                    if (robot.pos.equals(c)) {
-                        map.append("@");
-                    } else if (walls.contains(c)) {
-                        map.append("#");
-                    } else if (boxes.contains(c)) {
-                        map.append("O");
-                    } else {
-                        map.append(".");
-                    }
-                }
-                map.append("\n");
+        private Map<Coord, Box> computeBoxCoords() {
+            Map<Coord, Box> boxMap = new HashMap<>();
+            for (Box b : boxes) {
+                boxMap.put(b.left(), b);
+                boxMap.put(b.right(), b);
             }
-            map.append("\n");
-            System.out.println(map);
+            return boxMap;
         }
     }
 
@@ -123,5 +161,30 @@ public class WarehouseWoes {
             return moves.poll();
         }
 
+        Robot copy() {
+            return new Robot(pos(), new ArrayDeque<>(moves));
+        }
+
+        Robot move(Dir dir) {
+            return new Robot(pos().getAdjacent(dir), moves);
+        }
+
+    }
+
+    record Box(Coord left, Coord right) {
+        static Box from(Coord coord, int scalingFactor) {
+            if (scalingFactor == 2) {
+                return new Box(coord, coord.getAdjacent(Dir.RIGHT));
+            }
+            return new Box(coord, coord);
+        }
+
+        long gpsPosition() {
+            return left.row() * 100L + left.col();
+        }
+
+        Box move(final Dir dir) {
+            return new Box(left.getAdjacent(dir), right.getAdjacent(dir));
+        }
     }
 }
--- src/test/java/pl/lsobotka/adventofcode/year_2024/WarehouseWoesTest.java
+++ src/test/java/pl/lsobotka/adventofcode/year_2024/WarehouseWoesTest.java
@@ -23,7 +23,22 @@ class WarehouseWoesTest extends BaseTest {
     @MethodSource("firstStar")
     void firstStarTest(final String fileName, final long expected) {
         final List<String> lines = getFileInput(fileName);
-        final WarehouseWoes warehouseWoes = new WarehouseWoes(lines);
+        final WarehouseWoes warehouseWoes = WarehouseWoes.small(lines);
+        final long actual = warehouseWoes.sumOfBoxesCoordinate();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> secondStar() {
+        return Stream.of(Arguments.of("2024/WarehouseWoes_example_3", 618), //
+                Arguments.of("2024/WarehouseWoes_example_2", 9021), //
+                Arguments.of("2024/WarehouseWoes", 1548815));
+    }
+
+    @ParameterizedTest
+    @MethodSource("secondStar")
+    void secondStarTest(final String fileName, final long expected) {
+        final List<String> lines = getFileInput(fileName);
+        final WarehouseWoes warehouseWoes = WarehouseWoes.big(lines);
         final long actual = warehouseWoes.sumOfBoxesCoordinate();
         assertEquals(expected, actual);
     }
