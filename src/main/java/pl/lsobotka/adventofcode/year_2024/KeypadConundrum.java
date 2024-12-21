ac22e34b2358a43bc6850853d4fe7379c8a9f9bd
Łukasz Sobótka <sobotka.lukasz@gmail.com>
2024-12-21 23:32:46 +0100

    solution fo AoC 2024 Day 21: 'Keypad Conundrum' - work in progress

---
+++ src/main/java/pl/lsobotka/adventofcode/year_2024/KeypadConundrum.java
@@ -0,0 +1,241 @@
+package pl.lsobotka.adventofcode.year_2024;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import pl.lsobotka.adventofcode.utils.Coord;
+import pl.lsobotka.adventofcode.utils.Dir;
+import pl.lsobotka.adventofcode.utils.Rotate;
+
+/*
+ * https://adventofcode.com/2024/day/21
+ * */
+class KeypadConundrum {
+
+    private final Pattern PATTERN = Pattern.compile("^0*(\\d+)[A-Za-z]$");
+
+    private final List<String> numbers;
+    private final Control control;
+
+    KeypadConundrum(final List<String> numbers, final int controlQty) {
+        this.numbers = numbers;
+        this.control = prepareControls(controlQty);
+    }
+
+    long determineComplexity() {
+
+        final Map<String, List<Character>> pressedButtons = new TreeMap<>();
+        for (String number : numbers) {
+            final List<Character> buttonsToPress = number.chars().mapToObj(c -> (char) c).toList();
+            final List<Character> pressed = control.getButtons(buttonsToPress);
+            pressedButtons.put(number, pressed);
+        }
+
+        pressedButtons.forEach((k, v) -> System.out.println(
+                k + " = " + v.stream().map(String::valueOf).collect(Collectors.joining())));
+        pressedButtons.forEach((k, v) -> System.out.println(k + " = " + v.size()));
+
+        return pressedButtons.entrySet().stream().mapToLong(e -> toNumber(e.getKey()) * e.getValue().size()).sum();
+    }
+
+    private static Control prepareControls(int controlQty) {
+        final Keypad numeric = Keypad.numeric();
+        final Keypad control = Keypad.control();
+
+        final List<Control> controls = new ArrayList<>();
+
+        for (int i = 0; i < controlQty + 1; i++) {
+            if (i == 0) {
+                controls.add(new Control(numeric));
+            } else {
+                final Control next = new Control(control);
+                controls.add(next);
+                controls.get(i - 1).setNext(next);
+            }
+        }
+
+        return controls.getFirst();
+    }
+
+    private long toNumber(final String number) {
+        final Matcher matcher = PATTERN.matcher(number);
+        if (matcher.find()) {
+            return Long.parseLong(matcher.group(1));
+        }
+        return 0;
+    }
+
+    static class Control {
+        private static final char CONFIRM = 'A';
+        Control next;
+        final Keypad keypad;
+        char current;
+
+        public Control(Keypad keypad) {
+            this.keypad = keypad;
+            this.current = CONFIRM;
+        }
+
+        void setNext(Control next) {
+            this.next = next;
+        }
+
+        List<Character> getButtons(final List<Character> toPress) {
+            final List<Character> path = new ArrayList<>();
+
+            if (next == null) {
+                path.addAll(toPress);
+                current = toPress.getLast();
+            } else {
+                for (Character button : toPress) {
+                    final List<Character> currentPath = new ArrayList<>();
+                    if (current != button) {
+                        currentPath.addAll(keypad.getPath(current, button));
+                    }
+                    currentPath.add(CONFIRM);
+                    path.addAll(next.getButtons(currentPath));
+                    current = button;
+                }
+            }
+
+            return path;
+        }
+
+    }
+
+    record Keypad(List<Button> buttons, Map<Character, Map<Character, List<Character>>> paths) {
+        static Keypad numeric() {
+            final List<Button> buttons = new ArrayList<>();
+            buttons.add(new Button('7', Coord.of(0, 0)));
+            buttons.add(new Button('8', Coord.of(0, 1)));
+            buttons.add(new Button('9', Coord.of(0, 2)));
+            buttons.add(new Button('4', Coord.of(1, 0)));
+            buttons.add(new Button('5', Coord.of(1, 1)));
+            buttons.add(new Button('6', Coord.of(1, 2)));
+            buttons.add(new Button('1', Coord.of(2, 0)));
+            buttons.add(new Button('2', Coord.of(2, 1)));
+            buttons.add(new Button('3', Coord.of(2, 2)));
+            buttons.add(new Button('0', Coord.of(3, 1)));
+            buttons.add(new Button('A', Coord.of(3, 2)));
+            return new Keypad(buttons, calculatePaths(buttons));
+        }
+
+        static Keypad control() {
+            final List<Button> buttons = new ArrayList<>();
+            buttons.add(new Button('^', Coord.of(0, 1)));
+            buttons.add(new Button('A', Coord.of(0, 2)));
+            buttons.add(new Button('v', Coord.of(1, 1)));
+            buttons.add(new Button('<', Coord.of(1, 0)));
+            buttons.add(new Button('>', Coord.of(1, 2)));
+            return new Keypad(buttons, calculatePaths(buttons));
+        }
+
+        private static Map<Character, Map<Character, List<Character>>> calculatePaths(final List<Button> buttons) {
+            final Set<Coord> coords = buttons.stream().map(Button::coord).collect(Collectors.toSet());
+            final Map<Character, Map<Character, List<Character>>> shortest = new HashMap<>();
+
+            for (int first = 0; first < buttons.size() - 1; first++) {
+                for (int second = first + 1; second < buttons.size(); second++) {
+                    final Button start = buttons.get(first);
+                    final Button finish = buttons.get(second);
+
+                    if (shortest.getOrDefault(start.code, Collections.emptyMap()).containsKey(finish.code)) {
+                        continue;
+                    }
+
+                    final Queue<Path> paths = new PriorityQueue<>(Comparator.comparingInt(Path::changeCount));
+                    paths.add(Path.empty(start.coord));
+
+                    int bestChange = Integer.MAX_VALUE;
+
+                    while (!paths.isEmpty()) {
+                        final Path current = paths.poll();
+                        if (current.changeCount() >= bestChange) {
+                            continue;
+                        }
+
+                        if (current.coord.equals(finish.coord)) {
+                            final Map<Character, List<Character>> innerStart = shortest.computeIfAbsent(start.code,
+                                    v -> new HashMap<>());
+                            final Map<Character, List<Character>> innerFinish = shortest.computeIfAbsent(finish.code,
+                                    v -> new HashMap<>());
+
+                            innerStart.put(finish.code, toChars(current.dirs));
+                            innerFinish.put(start.code, toChars(reverseDirList(current.dirs)));
+                            bestChange = current.changeCount();
+                        } else {
+                            for (Dir dir : Dir.values()) {
+                                final Coord next = current.coord.getAdjacent(dir);
+                                if (coords.contains(next)) {
+                                    paths.add(current.next(next, dir));
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+            return shortest;
+        }
+
+        public List<Character> getPath(char start, char finish) {
+            return paths.getOrDefault(start, Collections.emptyMap()).getOrDefault(finish, Collections.emptyList());
+        }
+
+        private static List<Dir> reverseDirList(List<Dir> dirs) {
+            final List<Dir> reversed = new ArrayList<>();
+            for (Dir dir : dirs.reversed()) {
+                reversed.add(dir.rotate(Rotate.L).rotate(Rotate.L));
+            }
+
+            return reversed;
+        }
+
+        private static List<Character> toChars(List<Dir> dirs) {
+            final List<Character> chars = new ArrayList<>();
+            for (Dir dir : dirs) {
+                switch (dir) {
+                case UP -> chars.add('^');
+                case DOWN -> chars.add('v');
+                case LEFT -> chars.add('<');
+                case RIGHT -> chars.add('>');
+                }
+            }
+
+            return chars;
+        }
+
+        record Path(Coord coord, List<Dir> dirs) {
+            static Path empty(Coord start) {
+                return new Path(start, new ArrayList<>());
+            }
+
+            Path next(Coord coord, Dir dir) {
+                final List<Dir> next = new ArrayList<>(dirs);
+                next.add(dir);
+                return new Path(coord, next);
+            }
+
+            int changeCount() {
+                int change = 0;
+                if (!dirs.isEmpty()) {
+                    for (int i = 1; i < dirs.size(); i++) {
+                        if (dirs.get(i - 1) != dirs.get(i)) {
+                            change++;
+                        }
+                    }
+                }
+                return change;
+            }
+        }
+
+    }
+
+    record Button(char code, Coord coord) {
+    }
+
+}
---
+++ src/test/java/pl/lsobotka/adventofcode/year_2024/KeypadConundrumTest.java
@@ -0,0 +1,38 @@
+package pl.lsobotka.adventofcode.year_2024;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import pl.lsobotka.adventofcode.BaseTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeypadConundrumTest extends BaseTest {
+
+    private static Stream<Arguments> firstStar() {
+        return Stream.of( //
+                Arguments.of("2024/KeypadConundrum_example", 3, 126_384), //
+                Arguments.of("2024/KeypadConundrum_example_A", 1, 348), //
+                Arguments.of("2024/KeypadConundrum_example_A", 2, 812), //
+                Arguments.of("2024/KeypadConundrum_example_A", 3, 1_972), //
+                Arguments.of("2024/KeypadConundrum_example_B", 3, 58_800), //
+                Arguments.of("2024/KeypadConundrum_example_C", 3, 12_172), //
+                Arguments.of("2024/KeypadConundrum_example_D", 3, 29_184), //
+                Arguments.of("2024/KeypadConundrum_example_E", 3, 24_256), //
+                Arguments.of("2024/KeypadConundrum", 3, -1)); //164_988 to high
+    }
+
+    @ParameterizedTest
+    @MethodSource("firstStar")
+    void firstStarTest(final String fileName, int controlQty, final int expected) {
+        final List<String> lines = getFileInput(fileName);
+        final KeypadConundrum keypadConundrum = new KeypadConundrum(lines, controlQty);
+        final long actual = keypadConundrum.determineComplexity();
+        assertEquals(expected, actual);
+    }
+
+}
